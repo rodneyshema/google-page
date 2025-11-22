5935b1068bb687054cf12907f6579d05a8d9ed06
rodneyshema <rodneyshema2@gmail.com>
2025-11-22 18:30:50 +0200

    added my file

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('Hello')
+console.log('I like Pizza')
+
+// window.alert('This is an alert')
+window.alert('I like a pizza')
+
+document.getElementById('MyH!').textContent ='Hello' ;
+document.getElementById('MYp').textContent ='I like Pizza'
+let x;
+x=100;
+console.log(x);
+let price = 1000;
+let gpa = 2.1;
+console.log(gpa);
+console.log(price)
+
